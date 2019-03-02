af1e149408132b896a4cbf35c4a2eacf551e2dff
PraveenaDissanayake <praveenadissanayake8gmail.com>
2019-03-02 13:39:10 +0530

    Lab3

---
+++ AppContainer.jsx
@@ -0,0 +1,24 @@
+'use strict';
+import  React, {Component} from 'react';
+
+import Users from './Users';
+
+export default class  AppContainer extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            users: [{
+                id: Date.now(),
+                name: 'John'
+            }]
+        }
+    }
+
+    render(){
+        return <div>
+            <h2>Hello world</h2>
+            <Users users={this.state.users}/>
+        </div>;
+    }
+
+}
